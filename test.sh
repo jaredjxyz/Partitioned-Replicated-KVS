fdd6a003d73a96591984d3ebd72185855c1527e4
John Wilde <johnwildefire@gmail.com>
2017-03-19 19:40:46 -0700

    debugs

--- test.sh
+++ test.sh
@@ -8,9 +8,9 @@ tmux new-session -d -s pitted
 tmux split-window -t pitted 'docker run -p 8081:8080 --net=mynet --ip=10.0.0.21 -e K=2 -e VIEW="10.0.0.21:8080,10.0.0.22:8080" -e "IPPORT"="10.0.0.21:8080" hw4'
 tmux select-layout -t pitted main-horizontal
 tmux split-window -h -p 50 -t pitted  'docker run -p 8082:8080 --net=mynet --ip=10.0.0.22 -e K=2 -e VIEW="10.0.0.21:8080,10.0.0.22:8080" -e "IPPORT"="10.0.0.22:8080" hw4'
-tmux select-pane -U -t pitted
-tmux select-layout -t pitted tiled
-tmux select-pane -U -t pitted
+# tmux select-pane -U -t pitted
+# tmux select-layout -t pitted tiled
+# tmux select-pane -U -t pitted
 tmux set -t pitted mouse on
 tmux attach -t pitted
 
